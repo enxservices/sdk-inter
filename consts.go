1a041d46529c7c34189832fe923f4dfeeb8e2db5
Gabriel <gabriel.vinhais5@gmail.com>
2024-04-25 16:42:53 -0300

    feat: add endpoints

--- consts.go
+++ consts.go
@@ -1,6 +1,10 @@
 package intersdk
 
 const (
-	OauthUrl  = "https://cdpj.partners.bancointer.com.br/oauth/v2/token"
-	GrantType = "client_credentials"
+	BaseUrl      = "https://cdpj.partners.bancointer.com.br"
+	OauthUrl     = "https://cdpj.partners.bancointer.com.br/oauth/v2/token"
+	CobPixBoleto = "https://cdpj.partners.bancointer.com.br/cobranca/v3/cobrancas"
+	CobBoleto    = "https://cdpj.partners.bancointer.com.br/cobranca/v2/boletos"
+	CobPix       = "https://cdpj.partners.bancointer.com.br/pix/v2"
+	GrantType    = "client_credentials"
 )
