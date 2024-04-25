04dbff1389737795d1cfc2aad342a3394e44cd15
Gabriel <gabriel.vinhais5@gmail.com>
2024-04-25 16:43:19 -0300

    style: fix typo error

--- consts.go
+++ consts.go
@@ -1,10 +1,10 @@
 package intersdk
 
 const (
-	BaseUrl      = "https://cdpj.partners.bancointer.com.br"
-	OauthUrl     = "https://cdpj.partners.bancointer.com.br/oauth/v2/token"
-	CobPixBoleto = "https://cdpj.partners.bancointer.com.br/cobranca/v3/cobrancas"
-	CobBoleto    = "https://cdpj.partners.bancointer.com.br/cobranca/v2/boletos"
-	CobPix       = "https://cdpj.partners.bancointer.com.br/pix/v2"
-	GrantType    = "client_credentials"
+	BaseUrl         = "https://cdpj.partners.bancointer.com.br"
+	OauthUrl        = "https://cdpj.partners.bancointer.com.br/oauth/v2/token"
+	CobPixBoletoUrl = "https://cdpj.partners.bancointer.com.br/cobranca/v3/cobrancas"
+	CobBoletoUrl    = "https://cdpj.partners.bancointer.com.br/cobranca/v2/boletos"
+	CobPixUrl       = "https://cdpj.partners.bancointer.com.br/pix/v2"
+	GrantType       = "client_credentials"
 )
